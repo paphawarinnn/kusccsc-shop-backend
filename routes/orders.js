9020834deb36016f1284053d70a2bd1cab2878a3
paphawarinnn <paphawarin.sir@ku.th>
2026-05-01 16:21:14 +0700

    update backend

--- routes/orders.js
+++ routes/orders.js
@@ -33,6 +33,10 @@ const upload = multer({
 // =================== EMAIL ===================
 const transporter = nodemailer.createTransport({
   service: "gmail",
+  host: "smtp.gmail.com",
+  port: 587,
+  secure: false,
+  family: 4,
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS,
