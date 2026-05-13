bcf1d8668e1ba99f01c35438748a2581318dc684
paphawarinnn <paphawarin.sir@ku.th>
2026-05-13 15:22:36 +0700

    update backend

--- server.js
+++ server.js
@@ -31,3 +31,5 @@ const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
+const settingRoutes = require("./routes/settings");
+app.use("/api/settings", settingRoutes);
